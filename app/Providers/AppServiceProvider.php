a5ff573f840e713910d7319dc0ede2be1dac1f51
=deparagon <=cmasterpg7@gmail.com>
2016-12-05 15:50:36 +0000

    Updated commented line

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,15 +14,15 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-            //$categories = \App\Category::with('courses')->inRandomOrder()->take(6)->get();
-          // $subjects = \App\Course::inRandomOrder()->get();
-      //$states=["Abuja","Anambra","Enugu","Akwa-Ibom","Adamawa","Abia","Bauchi","Bayelsa","Benue","Borno","Cross-River","Delta","Ebonyi","Edo","Ekiti","Gombe","Imo","Jigawa","Kaduna","Kano","Katsina","Kebbi","Kogi","Kwara","Lagos","Nasarawa","Niger","Ogun","Ondo","Osun","Oyo","Plateau","Rivers","Sokoto","Taraba","Yobe","Zamfara"];
+            $categories = \App\Category::with('courses')->inRandomOrder()->take(6)->get();
+          $subjects = \App\Course::inRandomOrder()->get();
+      $states=["Abuja","Anambra","Enugu","Akwa-Ibom","Adamawa","Abia","Bauchi","Bayelsa","Benue","Borno","Cross-River","Delta","Ebonyi","Edo","Ekiti","Gombe","Imo","Jigawa","Kaduna","Kano","Katsina","Kebbi","Kogi","Kwara","Lagos","Nasarawa","Niger","Ogun","Ondo","Osun","Oyo","Plateau","Rivers","Sokoto","Taraba","Yobe","Zamfara"];
  
-      // View::share('maincategories', $categories);
-    //    View::share('states', $states);
-       //View::share('subjects', $subjects);
+      View::share('maincategories', $categories);
+       View::share('states', $states);
+       View::share('subjects', $subjects);
        if(Auth::user()){
-         //   View::share('profiledata', Auth::user());
+           View::share('profiledata', Auth::user());
        }
        
     }
