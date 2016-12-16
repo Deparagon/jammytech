6a8e0e942678d0f5c4f7c6795311f20bbf086989
adeniyimutia <mytinyblunt@gmail.com>
2016-12-16 15:46:37 +0100

    Added Lagos address

--- resources/views/externallayout.blade.php
+++ resources/views/externallayout.blade.php
@@ -102,6 +102,7 @@
         <div class="col-sm-3">
      <strong>ADDRESS</strong>
      <hr/>
+<p>16 Alhaja Eleshin Street, <br>Off Ramat Crescent, <br>Ogudu-Ojota, Kosofe, Lagos State.</p>
 <p>11, Obafemi Awolowo Way, <br>Beside BestSay Shopping Mall, <br>Igbona, Osogbo, Osun State. </p>
 
 
